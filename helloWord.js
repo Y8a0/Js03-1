adf433dc675f885a3e8b7e3ee2ff03ec1d82ebec
sillimanaborahma@gmail.com <sillimanaborahma@gmail.com>
2023-02-07 13:49:09 +0100

    commit2

--- helloWord.js
+++ helloWord.js
@@ -1,1 +1,8 @@
 console.log("Hello Word");
+var useName = "john";
+console.log(useName);
+console.log(5%3);
+console.log(25*6*5);
+var a = 10;
+var b = 20;
+console.log("New value of a:", b, "New value of b:", a);
