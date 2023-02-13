c6afca2128eb1c29c55feb0c3372337bca5a2ad7
sillimanaborahma@gmail.com <sillimanaborahma@gmail.com>
2023-02-13 10:57:28 +0100

    commit4

--- oneToTen.js
+++
@@ -1,5 +0,0 @@
-var sum = 0;
-for(let i = 1; i <= 10; i++){
- sum = sum + i;
-}
-console.log("sum of 1 to 10 :", sum);
--- weekendsVerified.js
+++
@@ -1,7 +0,0 @@
-let jour = 'Vendredi';
-if (jour === 'samedi' || jour === 'dimanche'){
-  console.log('weekend')
-}
-else{
-  console.log('weekday')
-}
