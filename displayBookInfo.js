6e11a1099ae8b9a9331734152448e8056e9421ef
sillimanaborahma@gmail.com <sillimanaborahma@gmail.com>
2023-02-14 18:53:53 +0100

    commit5

---
+++ displayBookInfo.js
@@ -0,0 +1,11 @@
+let book = {
+  title = "L' enfant Noir",
+  auteur = "Camara Laye",
+  annee = "1958",
+};
+function displayBookInfo(){
+  console.log('Titre:', book.title);
+  console.log('Auteur:', book.auteur);
+  console.log('Année:', book.annee);
+};
+displayBookInfo();
