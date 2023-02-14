b479b480ffa120c4643e136ece0af07f928599fe
Y8a0 <124534969+Y8a0@users.noreply.github.com>
2023-02-14 20:49:15 +0100

    Update displayPersonInfo.js

--- displayPersonInfo.js
+++ displayPersonInfo.js
@@ -2,7 +2,7 @@
 // Créer un objet personne avec des propriétés suivant : son nom Mariam et 30ans d'âge, et écrire une fonction qui affiche ces informations 
 let personne = {
   'nom' : 'Mariam',
-  'age' : '38',
+  'age' : '30',
 };
 function displayPersonInfo(){
   console.log(`Nom: ${personne.nom}`);
