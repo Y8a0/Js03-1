e40f3c2c51a956faa469c3b6f9d1a21234342f0e
sillimanaborahma@gmail.com <sillimanaborahma@gmail.com>
2023-02-14 19:03:02 +0100

    commit5

--- displayCompers.js
+++ displayCompers.js
@@ -1,8 +1,12 @@
+
 const codeloccol = {   
   name: 'Coder',    
   legs: 4,     
   tails: 1,     
   friends: ['Codeloccol Campers'],
 };
-codeloccole.name = 'Happy coder';
-console.log(codeloccol);
+function displayCompers(){
+  
+  console.log(codeloccol.friends)
+};
+displayCompers();
