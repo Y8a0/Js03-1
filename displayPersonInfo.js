08f47d64eb93a66a6156ae671cc0b1839f6869ea
sillimanaborahma@gmail.com <sillimanaborahma@gmail.com>
2023-02-14 18:42:25 +0100

    commit5

---
+++ displayPersonInfo.js
@@ -0,0 +1,9 @@
+let personne = {
+  'nom' : 'Mariam',
+  'age' : '38',
+};
+function displayPersonInfo(){
+  console.log(`Nom: ${personn.nom}`);
+  console.log(`Age: ${personne.age}`);
+};
+displayPersonInfo();
